1362d7359008af1ba2379c505a67cc195aa92d7a
Leo Peng <pengzimaobuaa@163.com>
2016-05-05 20:23:53 +0800

    add a better solution of one problem

--- Longest Valid Parentheses/main.cpp
+++ Longest Valid Parentheses/main.cpp
@@ -63,5 +63,27 @@ public:
         }
     }
 
-    // DP solutoin.
+    // DP solutoin. It runs 8ms.
+    int longestValidParentheses(string s) {
+        int len = (int)s.length();
+        vector<int> dp(len, 0);
+        int res = 0;
+        for (int i = 1; i < len; i++) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = i > 1 ? (dp[i - 2] + 2) : 2;
+                    res = max(res, dp[i]);
+                } else {
+                    if ((i - dp[i - 1] - 1 >= 0) && s[i - dp[i - 1] - 1] == '(') {
+                        dp[i] = dp[i - 1] + 2;
+                        if (i - dp[i - 1] - 2 >= 0) {
+                            dp[i] += dp[i - dp[i - 1] - 2];
+                        }
+                        res = max(res, dp[i]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
 };
