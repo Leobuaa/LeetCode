c2ffae43b48d9f75fdd249ffe64d9fd1b09724fc
Leo Peng <pengzimaobuaa@163.com>
2016-05-13 11:31:59 +0800

    Add recursive method
    A better one solution

--- Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal/main.cpp
@@ -35,4 +35,32 @@ public:
         TreeNode* res = buildTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
         return res;
     }
+
+    // Recursive method. It runs 16ms.
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* res = NULL;
+        if (preorder.size() <= 0) {
+            return res;
+        }
+        stack<TreeNode*> sta;
+        res = new TreeNode(preorder[0]);
+        sta.push(res);
+        int index = 0;
+        for (int i = 1; i < preorder.size(); i++) {
+            TreeNode* top = sta.top();
+            if (top->val != inorder[index]) {
+                top->left = new TreeNode(preorder[i]);
+                sta.push(top->left);
+            } else {
+                while (!sta.empty() && sta.top()->val == inorder[index]) {
+                    top = sta.top();
+                    sta.pop();
+                    index++;
+                }
+                top->right = new TreeNode(preorder[i]);
+                sta.push(top->right);
+            }
+        }
+        return res;
+    }
 };
