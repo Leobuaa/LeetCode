40dd2c838209efda285d51d3ffce118986428827
Leo Peng <pengzimaobuaa@163.com>
2016-05-30 19:33:39 +0800

    Add one method of one problem
    Add one more clear solution

--- Trapping Rain Water/main.cpp
+++ Trapping Rain Water/main.cpp
@@ -5,6 +5,7 @@ public:
  * @param  height [description]
  * @return        [description]
  */
+// Less coder but a little bit difficult to understand
 int trap(vector<int>& height) {
     int length = (int) height.size();
     int l = 0, r = length - 1;
@@ -14,6 +15,38 @@ int trap(vector<int>& height) {
         int lower = height[height[l] < height[r] ? l++ : r--];
         level = max(level, lower);
         res += (level - lower);
+    }
+    return res;
+}
+
+// More code but more clear
+int trap(vector<int>& height) {
+    int length = (int) height.size();
+    int l = 0, r = length - 1;
+    int maxLeft = 0, maxRight = 0;
+    int res = 0;
+    while (l <= r)
+    {
+        if (height[l] <= height[r])
+        {
+            if (height[l] > maxLeft)
+            {
+                maxLeft = height[l];
+            } else
+            {
+                res += maxLeft - height[l];
+            }
+            l++;
+        } else
+        {
+            if (height[r] > maxRight)
+            {
+                maxRight = height[r];
+            } else {
+                res += maxRight - height[r];
+            }
+            r--;
+        }
     }
     return res;
 }
