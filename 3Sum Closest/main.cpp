df82bc631895dcb09c2b82149f9602c8946832bf
Leo Peng <pengzimaobuaa@163.com>
2016-06-05 19:06:54 +0800

    Add one solution

---
+++ 3Sum Closest/main.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int result = 0;
+    int i = 0, j = int(nums.size()) - 1;
+    if (j == 2)
+        return nums[0] + nums[1] + nums[2];
+    int min = INT_MAX;
+    while (i < j - 1)
+    {
+        int l = i + 1, r = j;
+        
+         while (l < r)
+        {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum == target)  return target;
+            
+            if (abs(sum - target) < min)
+            {
+                result = sum;
+                min = abs(sum - target);
+            }
+            
+            (sum < target)? l++ : r--;
+        }
+
+        
+        i++;
+    }
+    
+    return result;
+
+    }
+};
