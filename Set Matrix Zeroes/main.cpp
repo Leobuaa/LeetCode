e04482848b7ec6088ee428c9419aaf201d70604e
Leo Peng <pengzimaobuaa@163.com>
2016-06-23 20:42:46 +0800

    Add one better solution

--- Set Matrix Zeroes/main.cpp
+++ Set Matrix Zeroes/main.cpp
@@ -34,4 +34,35 @@ public:
             }
         }
     }
+
+    // Use constant space solution
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size(), col = 0;
+        if (row > 0) {
+            col = matrix[0].size();
+        }
+        
+        int cols = 1;
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                cols = 0;
+            }
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (cols == 0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 };
