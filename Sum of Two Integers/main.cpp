1c5cb105cef11ee669bf1cea9f38985ba3e9e4ba
Leo Peng <pengzimaobuaa@163.com>
2016-07-09 23:06:53 +0800

    Add one more solution
    add one more solution

--- Sum of Two Integers/main.cpp
+++ Sum of Two Integers/main.cpp
@@ -6,4 +6,15 @@ public:
         }
         return getSum((a&b)<<1, a^b);
     }
+
+    int getSum(int a, int b) {
+        int sum = a | b, c = a & b;
+        while (c != 0) {
+            a = sum ^ c;
+            b = c << 1;
+            sum = a | b;
+            c = a & b;
+        }
+        return sum;
+    }
 };
