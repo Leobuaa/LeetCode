00488c73210d8b281d938ddebd1c143b190dd827
Leo Peng <pengzimaobuaa@163.com>
2016-07-10 11:25:08 +0800

    Add one more iterative method
    Add one more iterative method

--- Sum of Two Integers/main.cpp
+++ Sum of Two Integers/main.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+	// Recursive
     int getSum(int a, int b) {
         if (a == 0) {
             return b;
@@ -7,6 +8,7 @@ public:
         return getSum((a&b)<<1, a^b);
     }
 
+    // Iterative 
     int getSum(int a, int b) {
         int sum = a | b, c = a & b;
         while (c != 0) {
@@ -15,6 +17,17 @@ public:
             sum = a | b;
             c = a & b;
         }
+        return sum;
+    }
+
+    // Iterative 
+    int getSum(int a, int b) {
+        int sum = 0;
+        do {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }while (b != 0);
         return sum;
     }
 };
