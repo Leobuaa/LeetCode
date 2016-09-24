a8c5bb8ae976168c4c78e6283c248b2f491e07a0
Leo Peng <616690602@qq.com>
2016-09-24 20:30:00 +0800

    add one solution

---
+++ Binary Watch/main.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+        vector<string> readBinaryWatch(int num) {
+            vector<string> res;
+            const int H = 12;
+            const int M = 60;
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < M; j++) {
+                    int count = 0;
+                    int hour = i, minute = j;
+                    while (hour > 0) {
+                        count += hour & 1;
+                        hour >>= 1;
+                    }
+
+                    while (minute > 0) {
+                        count += minute & 1;
+                        minute >>= 1;
+                    }
+
+                    if (count == num) {
+                        string temp = to_string(i) + ":" + ((j < 10) ? "0" : "") + to_string(j);
+                        res.push_back(temp);
+                    }
+                }
+            }
+
+            return res;
+        }
+};
