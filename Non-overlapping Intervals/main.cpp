1bf5b5855c4516a89b9f48bbc118178ee3208c88
Leo Peng <pengzimaobuaa@163.com>
2016-11-29 08:44:21 +0800

    Add one solution, Greedy

---
+++ Non-overlapping Intervals/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<Interval>& intervals) {
+        if (intervals.size() <= 0) {
+            return 0;
+        }
+        
+        vector<Interval> res;
+        vector<Interval> copyIntervals(intervals);
+        auto comp = [](Interval x, Interval y) {
+            return x.end < y.end || (x.end == y.end && x.start > y.start);
+        };
+        
+        sort(copyIntervals.begin(), copyIntervals.end(), comp);
+        res.push_back(copyIntervals[0]);
+        for (int i = 1; i < copyIntervals.size(); i++) {
+            Interval tmp = res.back();
+            if (copyIntervals[i].start >= tmp.end) {
+                res.push_back(copyIntervals[i]);
+            }
+        }
+        
+        return copyIntervals.size() - res.size();
+    }
+};
