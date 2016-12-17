642bb402c82dab9d9a507c5141e811ac8b670a4c
Leo Peng <pengzimaobuaa@163.com>
2016-12-17 20:37:57 +0800

    Add one solution

---
+++ Elimination Game/main.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        return (n / 2 - lastRemaining(n / 2) + 1) * 2;
+    }
+};
