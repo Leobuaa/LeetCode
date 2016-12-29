cc8d4371e86111330412ccb79797f44918eb4e19
Leo Peng <pengzimaobuaa@163.com>
2016-12-29 12:40:30 +0800

    add one more solution

--- UTF-8 Validation/main.cpp
+++ UTF-8 Validation/main.cpp
@@ -34,4 +34,32 @@ public:
         
         return cnt == 0;
     }
+
+    // less code
+    bool validUtf8(vector<int>& data) {
+        int cnt = 0;
+        for (int num : data) {
+            if (num < 0 || num >= 248) {
+                return false;
+            }
+            
+            if (num >= 128 && num < 192) {
+                if (cnt == 0) return false;
+                cnt--;
+            } else if (num >= 192){
+                if (cnt > 0) return false;
+                int mask = 32;
+                cnt = 1;
+                while (mask & num) {
+                    mask >>= 1;
+                    cnt++;
+                }
+            } else {
+                if (cnt > 0) return false;
+                cnt = 0;
+            }
+        }
+        
+        return cnt == 0;
+    }
 };
