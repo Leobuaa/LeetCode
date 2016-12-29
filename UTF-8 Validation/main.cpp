dc218339e1adfadfaecc9bffff69b70ccf714ef9
Leo Peng <pengzimaobuaa@163.com>
2016-12-29 12:48:33 +0800

    add one more solution

--- UTF-8 Validation/main.cpp
+++ UTF-8 Validation/main.cpp
@@ -62,4 +62,22 @@ public:
         
         return cnt == 0;
     }
+
+    // brilliant solution.
+    bool validUtf8(vector<int>& data) {
+        int cnt = 0;
+        for (int num : data) {
+            if (cnt == 0) {
+                if ((num >> 5) == 0b110) cnt = 1;
+                else if ((num >> 4) == 0b1110) cnt = 2;
+                else if ((num >> 3) == 0b11110) cnt = 3;
+                else if (num >> 7) return false;
+            } else {
+                if ((num >> 6) != 0b10) return false;
+                cnt--;
+            }
+        }
+        
+        return cnt == 0;
+    }
 };
